d6586de7e1a61ec81089cc2912efd0199f813680
SamiSousa <ssshahin@bu.edu>
2018-02-24 15:37:07 -0500

    trying a new js test

--- scripts/main.js
+++ scripts/main.js
@@ -10,4 +10,8 @@ var myHeading = document.querySelector('h1');
 
 myHeading.textContent = "Hello world";
 
+myHeading.onclick = function(){
+	alert("Ouch! Stop poking me!");
+}
+
 alert("6 times 7 is " + multiply(6, 7));
