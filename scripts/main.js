6b227b7d06529da4abfdc7b9bda280733d75a925
SamiSousa <ssshahin@bu.edu>
2018-02-24 15:45:56 -0500

    trying a new js test

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,13 @@
 
 
-function multiply(num1, num2){
-	var result = num1 * num2;
-	return result;
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'img/smile.png'){
+		myImage.setAttribute ('src', 'img/ProfessionalCandid_large.png');
+	} else {
+		myImage.setAttribute('src', 'img/smile.png');
+	}
 }
 
-
-var myHeading = document.querySelector('h1');
-
-myHeading.textContent = "Hello world";
-
-myHeading.onclick = function(){
-	alert("Ouch! Stop poking me!");
-}
-
-alert("6 times 7 is " + multiply(6, 7));
