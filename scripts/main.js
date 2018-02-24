92d0c88ad34d571d6bd2646330ee2353c533b688
SamiSousa <ssshahin@bu.edu>
2018-02-24 15:29:42 -0500

    adding test page for javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
 
+
+function multiply(num1, num2){
+	var result = num1 * num2;
+	return result;
+}
+
+
 var myHeading = document.querySelector('h1');
 
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = "Hello world";
 
-alert("Hi there");
+alert("6 times 7 is " + multiply(6, 7));
